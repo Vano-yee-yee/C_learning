620c246713a33702ebae10181619700abbc090d7
Vano-yee-yee <vanya190514@gmail.com>
2024-02-29 20:37:17 +0300

    Header file for LESs

---
+++ Headers/matrix.h
@@ -0,0 +1,25 @@
+#pragma once
+
+/**
+* @brief Вычисляет определитель матрицы
+* @param matrix_: квадратная матрица
+* @param n_: порядок матрицы
+* @return действительное число
+*/
+double det(const double* matrix_, const unsigned int n_);
+
+/**
+* @brief Находит обратную матрицу
+* @param matrix_: квадратная матрица (массив чисел)
+* @param n_: порядок матрицы
+* @return массив чисел
+*/
+double* reverse_matrix(const double* matrix_, const unsigned int n_);
+
+/**
+* @brief Решает СЛУ методом Крамера
+* @param coefficients_: коэффициенты при неизвестных и свободные члены
+* @param unknowns_: количество неизвестных
+* @return массив значений неизвестных (слева-направо)
+*/
+double* Kramer_LES(const double* coefficients_, const unsigned int unknowns_);
