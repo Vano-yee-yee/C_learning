1ebee1e7079364d5eb5a699073245eb22dfbfac3
Vano-yee-yee <119596485+Vano-yee-yee@users.noreply.github.com>
2024-06-26 00:18:49 +0300

    Delete headers directory

--- headers/cows and bulls.h
+++
@@ -1,8 +0,0 @@
-#if !defined(COWS_AND_BULLS_H)
-#define COWS_AND_BULLS_H
-/**
-* @brief Начинает игру "Быки и коровы"
-* @param dig_: количество цифр в числе [1, 10]
-*/
-void start_CaB(const unsigned char dig_);
-#endif
